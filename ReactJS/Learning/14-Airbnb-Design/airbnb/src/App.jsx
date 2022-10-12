9ca2a53c84f6c3b41211cde4b5f0a0d5bc275e4d
Regiv <giverbro13@gmail.com>
2022-10-12 14:48:12 +0545

    airbnb

--- ReactJS/Learning/14-Airbnb-Design/airbnb/src/App.jsx
+++ ReactJS/Learning/14-Airbnb-Design/airbnb/src/App.jsx
@@ -1,24 +1,35 @@
-import './App.css'
-import Nav from './components/navbar'
-import Hero from './components/hero'
-import Card from './components/card'
+import "./App.css";
+import Nav from "./components/navbar";
+import Hero from "./components/hero";
+import Card from "./components/card";
+import data from "./utils/data";
 
 function App() {
+	let cards = data.map((elem) => {
+		return (
+			<Card
+				// Create key just to make the item unique. The `id` is provided by database
+				key={elem.id}
+				// title= {elem.title}
+				// price= {elem.price}
+				// coverImg= {elem.coverImg}
+				// rating={elem.stats.rating}
+				// reviewCount={elem.stats.reviewCount}
+				// location={elem.location}
+				// openSpots={elem.openSpots}
 
-  return (
-    <div className="App">
-        <Nav/>
-        <Hero/>
-        <Card
-        person="../../../public/assets/katie-zaferes.png"
-        rating={5.0}
-        review={6}
-        country="USA"
-        message="Life Lessons with Katie Zaferes"
-        cost="5.6 USD"
-        />
-    </div>
-  )
+				// Instead of long code above, directly assign the object
+				item={elem}
+			/>
+		);
+	});
+	return (
+		<div className="App">
+			<Nav />
+			<Hero />
+			<div className="box">{cards}</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- ReactJS/Learning/14-Airbnb-Design/airbnb/src/components/card/index.jsx
+++ ReactJS/Learning/14-Airbnb-Design/airbnb/src/components/card/index.jsx
@@ -3,18 +3,22 @@ import star from "../../../public/assets/star.png"
 import "./style.css"
 
 export default function Card(props) {
+    let badgeContent;
+    if(props.item.openSpots == 0) badgeContent = "SOLD OUT";
+    else if(props.item.location == "Online") badgeContent = "ONLINE";
+
     return (
         <div className="card">
-            <img src={props.coverImg} className="card__image" />
+            {badgeContent && <div className="card__badge">{badgeContent}</div>}
+            <img src={`../../../public/assets/${props.item.coverImg}`} className="card__image" />
             <div className="card__info">
                 <img src={star} className="card__star" />
-                <span>{props.stats.rating}</span>
-                <span className="gray">({props.stats.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p>{props.description}</p>
-            <p><span className="bold">{props.price}</span> / person</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">{props.item.price}</span> / person</p>
         </div>
     )
 }
